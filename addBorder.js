b87ac25933570f51fca77adc5cb25ead6f25da06
Pijush Chakraborty <piyushchakraborty9@gmail.com>
2019-11-16 23:44:24 +0530

    addBorder.js

---
+++ addBorder.js
@@ -0,0 +1,26 @@
+// function addBorder(pic) {
+//     const lenghtOfWall = pic[0].length + 2;
+//     let wall = '';
+//     for(let i = 0 ; i < lenghtOfWall ; i++){
+//         wall =wall.concat('*');
+//     }
+//     pic.unshift(wall);
+//     pic.push(wall);
+//     for(let i = 1 ; i < pic.length - 1; i++){
+//         pic[i] = '*'.concat(pic[i],'*');
+//     }
+//     return pic;
+// }
+// solution 2 using repeat
+
+function addBorder(pic){
+    const wall = '*'.repeat(pic[0].length + 2)
+    pic.unshift(wall);
+    pic.push(wall);
+    for (let i = 1; i < pic.length - 1; i++) {
+        pic[i] = '*'.concat(pic[i],'*');
+    }
+    return pic;
+}
+
+console.log(addBorder(["abc","def"]));
