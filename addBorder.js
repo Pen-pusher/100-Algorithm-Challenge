5db910a903f35a9b2e4fe6f6f1f5864a5c10b479
Pijush Chakraborty <piyushchakraborty9@gmail.com>
2019-11-16 23:48:04 +0530

    rectified error

--- addBorder.js
+++ addBorder.js
@@ -1,16 +1,19 @@
-// function addBorder(pic) {
-//     const lenghtOfWall = pic[0].length + 2;
-//     let wall = '';
-//     for(let i = 0 ; i < lenghtOfWall ; i++){
-//         wall =wall.concat('*');
-//     }
-//     pic.unshift(wall);
-//     pic.push(wall);
-//     for(let i = 1 ; i < pic.length - 1; i++){
-//         pic[i] = '*'.concat(pic[i],'*');
-//     }
-//     return pic;
-// }
+
+// given a rectangular matrix of characters ,add a border of asterisks(*) to it
+
+function addBorder(pic) {
+    const lenghtOfWall = pic[0].length + 2;
+    let wall = '';
+    for(let i = 0 ; i < lenghtOfWall ; i++){
+        wall =wall.concat('*');
+    }
+    pic.unshift(wall);
+    pic.push(wall);
+    for(let i = 1 ; i < pic.length - 1; i++){
+        pic[i] = '*'.concat(pic[i],'*');
+    }
+    return pic;
+}
 // solution 2 using repeat
 
 function addBorder(pic){
