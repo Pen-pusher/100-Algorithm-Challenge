03f380691af15d2f078941cea3eafacb149aac5b
Pijush Chakraborty <piyushchakraborty9@gmail.com>
2019-11-25 21:49:39 +0530

    alphabetSubSequenceShift

---
+++ subsequence.js
@@ -0,0 +1,17 @@
+// check whether the given string is a aubsequence of the plaintext alphabet.
+
+
+function alphabetSubSequence(s){
+  const chars = [] = s.split('');
+  const charValues = []=[];
+   
+  chars.forEach(char => {
+      charValues.push(char.charCodeAt(0))
+  });
+
+  console.log(new Set(charValues))
+
+  return true;
+}   
+
+console.log(alphabetSubSequence('efg'))
