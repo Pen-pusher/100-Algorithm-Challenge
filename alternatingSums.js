20bf8d430cd1d73fc3e66fbd83afdd5ed6eb1f1d
Pijush Chakraborty <piyushchakraborty9@gmail.com>
2019-11-26 22:51:49 +0530

    alternateSums

---
+++ alternatingSums.js
@@ -0,0 +1,18 @@
+function alternate(a) {
+    let even=0;
+    let odd = 0;
+        a.forEach((element,index) => {
+             if(index % 2 == 0){
+                even += element
+        //  console.log(element);
+     }
+        else{
+           odd += element;
+        //  console.log(element);
+     }
+ });
+    return [even, odd];
+
+}
+
+console.log(alternate([30, 20, 40, 50, 60]))
