fa777f652da328dbd84360e1a111194ff9312e19
Pijush Chakraborty <piyushchakraborty9@gmail.com>
2019-12-02 21:48:58 +0530

    error - fixed

--- array-previous-less.js
+++ array-previous-less.js
@@ -1,16 +1,19 @@
-function arrayPreviousLess(items){
-    const lessThanList= [];
+// given an array of integers, for each position i, search among the previous positions for the last (from the left) position that contains a smaller
+// store this value at position i in the answer.if no such value can be found, store-1 instead.
 
-    for(let i =items.length - 1; i >= 0; i--){
-        for(let j =i; j >=0; j--){
-            if(items[i] > items[j]){
-                lessThanList.unshift(items[j]);
-                break;
-            }else if(j===0){
-                lessThanList.unshift(-1);
-            }
-        }
+function arrayPreviousLess(items) {
+  const lessThanList = [];
+
+  for (let i = items.length - 1; i >= 0; i--) {
+    for (let j = i; j >= 0; j--) {
+      if (items[i] > items[j]) {
+        lessThanList.unshift(items[j]);
+        break;
+      } else if (j === 0) {
+        lessThanList.unshift(-1);
+      }
     }
-    return lessThanList;
+  }
+  return lessThanList;
 }
-console.log(arrayPreviousLess([3,5,2,4,5]));
+console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
